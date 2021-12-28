6f3d2bbb2b96dc31af3ba74ce409b93aa73a942e
moeinmir <moein.mirani@gmail.com>
2021-12-28 21:41:29 +0330

    14001007 admin costumized

--- shop/sell/admin.py
+++ shop/sell/admin.py
@@ -4,7 +4,48 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Shop)
-admin.site.register(ListOfComodity)
-admin.site.register(ShopBasket)
-admin.site.register(Order)
+# admin.site.register(Shop)
+# admin.site.register(ListOfComodity)
+# admin.site.register(ShopBasket)
+# admin.site.register(Order)
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    list_filter = ('name', 'status')
+    search_fields = ('name',)
+    fields = (('name', 'owner'), 'category')
+    date_hierarchy = ('created_on')
+
+
+admin.site.register(Shop, ShopAdmin)
+
+
+class ListOfComodityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    list_filter = ('name', 'status')
+    search_fields = ('name',)
+    date_hierarchy = ('created_on')
+
+
+admin.site.register(ListOfComodity, ListOfComodityAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('costumer', 'comodity')
+    list_filter = ('comodity', 'shop_basket')
+    search_fields = ('costumer',)
+    date_hierarchy = ('created_on')
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class ShopBasketAdmin(admin.ModelAdmin):
+    list_display = ('costumer', 'status')
+    list_filter = ('costumer', 'status')
+    search_fields = ('costumer',)
+    date_hierarchy = ('created_on')
+
+
+admin.site.register(ShopBasket, ShopBasketAdmin)
